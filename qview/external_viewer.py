155fcfa5af765835e2a4dc6232148b9645800ed7
Anders Brakestad <anders.brakestad>
2021-01-13 13:06:29 +0100

    added refresh button

--- qview/external_viewer.py
+++ qview/external_viewer.py
@@ -14,6 +14,12 @@ class ExternalViewer(tk.Toplevel):
         self.frame = tk.Frame(self)
         self.frame.pack(fill=tk.BOTH, expand=True)
 
+        tk.Button(self, text='Refresh', command=self.refresh).pack()
+
         self.viewer = QueueViewer(self.frame)
         self.viewer.pack(fill=tk.BOTH, expand=True)
+        self.viewer.display_file(self.qhandler, self.pid, self.ftype, skip_to_end=self.skip_to_end)
+
+    def refresh(self):
+        self.viewer.delete(1.0, tk.END)
         self.viewer.display_file(self.qhandler, self.pid, self.ftype, skip_to_end=self.skip_to_end)
